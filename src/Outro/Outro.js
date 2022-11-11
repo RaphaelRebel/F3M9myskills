3296c8a8ed6ac72a9bef07f3339d1970b718d811
Raphael Rebel <raphaelrebel@live.com>
2022-11-11 15:31:09 +0100

    Finish 11 November 2022 15:31

--- src/Outro/Outro.js
+++ src/Outro/Outro.js
@@ -26,16 +26,33 @@ const Outro = (props) => {
          let questions = []
          let ratings = []
          props.questionsAndAnswers.map(question => {
-                  questions.push(question.question)
+                  questions.push("Vraag #"+ question.number)
                   ratings.push(question.rating)
          })
          const labels = questions;
-         console.log(props.questionsAndAnswers)
          let data = {
                   labels: labels,
                   datasets: [{
-                           label: 'My First Dataset',
-                           data: ratings
+                           label: 'Mijn score',
+                           data: ratings,
+                           backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)',
+                                    'rgba(255, 205, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(201, 203, 207, 0.2)'
+                                  ],
+                                  borderColor: [
+                                    'rgb(255, 99, 132)',
+                                    'rgb(255, 159, 64)',
+                                    'rgb(255, 205, 86)',
+                                    'rgb(75, 192, 192)',
+                                    'rgb(54, 162, 235)',
+                                    'rgb(153, 102, 255)',
+                                    'rgb(201, 203, 207)'
+                                  ],
                   }]
          }
          return (
